cfe03e21735956840634e62faed096775f8d66f4
Tausif <tausif.akram@citruspay.com>
2018-02-08 15:57:46 +0530

    PayLoad dumped from Voyager

--- worker/enquiry.go
+++ worker/enquiry.go
@@ -3,15 +3,18 @@ package main
 import (
 	"log"
 	"github.com/aws/aws-lambda-go/lambda"
+	lambdaService "github.com/aws/aws-sdk-go/service/lambda"
 )
 
 /*
     @author: tausif
 */
 
-func EnquiryHandler() error {
+func EnquiryHandler(output *lambdaService.InvokeOutput) error {
 
 	log.Print("Executing the Enquiry Worker")
+
+	log.Print("Payload received: " , output.Payload)
 
 
 	log.Print("Processing completed")
