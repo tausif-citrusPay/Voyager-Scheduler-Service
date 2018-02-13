804d45450839398b28fc9b0b9a9acab3a2ab8da1
Tausif <tausif.akram@citruspay.com>
2018-02-13 12:59:01 +0530

    Added API call to BLAZPAY VERSION

--- worker/enquiry.go
+++ worker/enquiry.go
@@ -1,26 +1,54 @@
 package main
 
 import (
-	"log"
 	"github.com/aws/aws-lambda-go/lambda"
-	lambdaService "github.com/aws/aws-sdk-go/service/lambda"
+	"log"
+	"net/http"
 )
 
 /*
-    @author: tausif
+   @author: tausif
 */
 
 func EnquiryHandler(output string) error {
 
 	log.Print("Executing the Enquiry Worker")
 
-	log.Print("Payload received: " , output)
-	log.Print("Output: " , lambdaService.InvokeInput{}.Payload)
+	log.Print("Payload received: ", output)
+	url := "https://pay.blazepay.in/info"
+
+	// Build the request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return err
+	}
+
+	// For control over HTTP client headers,
+	// redirect policy, and other settings,
+	// create a Client
+	// A Client is an HTTP client
+	client := &http.Client{}
+
+	// Send the request via a client
+	// Do sends an HTTP request and
+	// returns an HTTP response
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return err
+	}
+
+	log.Print("BLAZEPAY VERSION :", resp)
+
+	// Callers should close resp.Body
+	// when done reading from it
+	// Defer the closing of the body
+	defer resp.Body.Close()
 
 
 	log.Print("Processing completed")
 	return nil
-
 
 }
 
